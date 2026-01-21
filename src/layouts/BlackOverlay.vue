dcdcaa2b9e2d1192ef791df07f1f2ccf3fe77305
Stefan <stefan@alphaorange.de>
2026-01-21 22:36:16 +0100

    First implementation of a warning overlay before overwriting book savegames - needs some tweaking

---
+++ src/layouts/BlackOverlay.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="layer">
+    <div class="content">
+      <slot></slot>
+      <br />
+      <button @click="close">Okay</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['close'])
+const close = () => {
+  emit('close')
+}
+</script>
+
+<style scoped>
+.layer {
+  position: absolute;
+  top: -1rem;
+  /* TODO: there must be a cleaner solution */
+  left: -1rem;
+  z-index: 9999;
+  width: calc(100% + 2rem);
+  height: calc(100vh);
+  margin: 0;
+  border-width: 0;
+  background: var(--bg-darkshade);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.content {
+  max-width: 50%;
+  background: var(--bg-page);
+  padding: 1rem;
+  border-radius: 0.5rem;
+}
+button {
+}
+</style>
--- src/views/TheHomeView.vue
+++ src/views/TheHomeView.vue
@@ -1,24 +1,43 @@
 <template>
   <TheMainLayout>
     <template #default>
-      <div class="page-header">
-        <h1>Book Shelf</h1>
-      </div>
-      <div class="shelf horizontal-flex item-selection">
-        <SavegameCover v-if="shelf.hasSaveData" :saveData="shelf.saveData" @click="loadSavegame" />
-        <BookInfoCover
-          v-for="book in shelf.books"
-          :key="book.id"
-          :book="book"
-          @click="loadBook(book)"
-        />
+      <div class="content">
+        <div class="page-header">
+          <h1>Book Shelf</h1>
+        </div>
+        <div class="shelf horizontal-flex item-selection">
+          <SavegameCover
+            v-if="shelf.hasSaveData"
+            :saveData="shelf.saveData"
+            @click="loadSavegame"
+          />
+          <BookInfoCover
+            v-for="book in shelf.books"
+            :key="book.id"
+            :book="book"
+            @click="loadBook(book)"
+          />
+        </div>
+        <BlackOverlay v-if="showOverlay" @close="closeOverlay">
+          <template #default>
+            <div class="warning">
+              <h3>One Second ...</h3>
+              <div class="warning-message">
+                The book '{{ bookStore.title }}' is currently loaded. If you chose another book now
+                (or even the same book), all unsaved progress will be lost!
+              </div>
+            </div>
+          </template>
+        </BlackOverlay>
       </div>
     </template>
   </TheMainLayout>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
 import TheMainLayout from '@/layouts/TheMainLayout.vue'
+import BlackOverlay from '@/layouts/BlackOverlay.vue'
 import BookInfoCover from '@/components/BookInfoCover.vue'
 import SavegameCover from '@/components/SavegameCover.vue'
 
@@ -39,10 +58,24 @@ const loadSavegame = async () => {
   shelf.loadBook()
   router.push('/book')
 }
+
+const askOverlay = ref(true)
+const showOverlay = computed(() => {
+  return bookStore.loaded && askOverlay.value
+})
+const closeOverlay = () => {
+  askOverlay.value = false
+}
 </script>
 
 <style scoped>
+.content {
+  position: relative;
+}
 .shelf {
   justify-content: center;
+}
+.warning {
+  text-align: center;
 }
 </style>
