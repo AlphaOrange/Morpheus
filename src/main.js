3386efbcdefbc5514c75646ecfe9545af2fa0cac
Stefan Tewes <stefan.tewes@rtl.de>
2026-07-19 12:18:26 +0200

    move font awesome import into plugin file to not bloat main.js

--- src/main.js
+++ src/main.js
@@ -10,57 +10,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import { useShelfStore } from '@/stores/shelf'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-  faArrowLeft,
-  faBan,
-  faBookBookmark,
-  faBookmark,
-  faBookOpenReader,
-  faBuildingColumns,
-  faCircleInfo,
-  faCirclePlay,
-  faCircleQuestion,
-  faCircleXmark,
-  faComment,
-  faComments,
-  faDoorOpen,
-  faFlag,
-  faHourglassHalf,
-  faMaximize,
-  faMoon,
-  faPersonWalking,
-  faSignature,
-  faSliders,
-  faStar,
-} from '@fortawesome/free-solid-svg-icons'
-library.add(
-  faArrowLeft,
-  faBan,
-  faBookBookmark,
-  faBookmark,
-  faBookOpenReader,
-  faBuildingColumns,
-  faCircleInfo,
-  faCirclePlay,
-  faCircleQuestion,
-  faCircleXmark,
-  faComment,
-  faComments,
-  faDoorOpen,
-  faFlag,
-  faHourglassHalf,
-  faMaximize,
-  faMoon,
-  faPersonWalking,
-  faSignature,
-  faSliders,
-  faStar,
-)
+import { FontAwesomeIcon } from '@/plugins/fontawesome'
 
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
 app.component('font-awesome-icon', FontAwesomeIcon)
---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,53 @@
+// src/plugins/fontawesome.js
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faArrowLeft,
+  faBan,
+  faBookBookmark,
+  faBookmark,
+  faBookOpenReader,
+  faBuildingColumns,
+  faCircleInfo,
+  faCirclePlay,
+  faCircleQuestion,
+  faCircleXmark,
+  faComment,
+  faComments,
+  faDoorOpen,
+  faFlag,
+  faHourglassHalf,
+  faMaximize,
+  faMoon,
+  faPersonWalking,
+  faSignature,
+  faSliders,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faArrowLeft,
+  faBan,
+  faBookBookmark,
+  faBookmark,
+  faBookOpenReader,
+  faBuildingColumns,
+  faCircleInfo,
+  faCirclePlay,
+  faCircleQuestion,
+  faCircleXmark,
+  faComment,
+  faComments,
+  faDoorOpen,
+  faFlag,
+  faHourglassHalf,
+  faMaximize,
+  faMoon,
+  faPersonWalking,
+  faSignature,
+  faSliders,
+  faStar,
+)
+
+export { FontAwesomeIcon }
